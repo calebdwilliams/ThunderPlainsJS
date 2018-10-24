854fd8ff5ae3311a1e5750323ee1ac15c45f88f4
Caleb Williams <caleb.williams@capitalone.com>
2018-10-24 15:04:50 -0500

    chore: Mark reminded me to commit

--- docs/presentation.js
+++ docs/presentation.js
@@ -4,3 +4,9 @@ import './components/tp-slides.js';
 
 /** Demos */
 import './demos/custom-elements.js';
+
+window.TEMPLATE_DEMO = (target) => {
+    const node = document.getElementById('my-template');
+    const clone = document.importNode(node.content, true);
+    target.after(clone);
+};
